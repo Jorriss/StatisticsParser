006cbb7933555c83bd9d655dc689b38af92f98d9
Richie Rump <jorriss@gmail.com>
2014-04-29 23:01:53 -0400

    Add language dropdown to the navbar.

--- src/assets/js/statsioparser.js
+++ src/assets/js/statsioparser.js
@@ -7,6 +7,16 @@ function includeExample(value, lang) {
     }
 }
 
+function updateQueryStringParameter(uri, key, value) {
+    var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+    var separator = uri.indexOf('?') !== -1 ? "&" : "?";
+    if (uri.match(re)) {
+        return uri.replace(re, '$1' + key + "=" + value + '$2');
+    } else {
+        return uri + separator + key + "=" + value;
+    }
+}
+
 function toggleCheckmark(checkmarkSpan, cookieKey) {
     if($.cookies.get(cookieKey) === true) {
         $.cookies.set(cookieKey, "false");
